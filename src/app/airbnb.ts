c46c83cd841c927756689ab01171c9199ef2a8de
OmarReyesRmz <reyesomar028@gmail.com>
2024-04-22 17:48:45 -0600

    update casas

--- src/app/airbnb.ts
+++ src/app/airbnb.ts
@@ -15,6 +15,7 @@ export const CASAS:Casas[] = [
         calificacion: 5,
         anfitrion: "Martha",
         descripcion: "Hermosa casa recién remodelada con amplios jardines en 1,800 m2, alberca con calefacción solar, porterías de futbol y red de voleibol disponible, capacidad para 11 personas en 4 recámaras y 3 baños completos, amplia terraza con linda barra y vista al jardín para disfrutar con la familia y amigos. Es Pet Friendly y cuenta con facilidades para personas con capacidades limitadas. Ubicada en una zona tranquila sin tráfico, cerca de tiendas de autoservicio. Se renta por un mínimo de 2 noches.",
-        servicios: "Cocina Wifi Área para trabajar Estacionamiento residencial gratuito en las instalaciones: 4 espacios Alberca: Uso privado, al aire libre, disponible todo el año, disponible las 24 horas, climatizada"
+        servicios: "Cocina Wifi Área para trabajar Estacionamiento residencial gratuito en las instalaciones: 4 espacios Alberca: Uso privado, al aire libre, disponible todo el año, disponible las 24 horas, climatizada",
+        precio: 3500
     }
 ];
