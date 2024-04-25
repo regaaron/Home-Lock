4182b2cf2af942c8644aea3aeef279edceb648db
AL4NK <alan22518prog@gmail.com>
2024-04-25 14:17:30 -0600

    Home
    Pagina de home

--- src/app/componentes/descubre/descubre.component.ts
+++ src/app/componentes/descubre/descubre.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Casas } from '../../casas';
+import { CasasService } from '../../servicios/casas.service';
+import { RouterModule } from '@angular/router';
+import { TruncateTextPipe } from "../../truncate-text.pipe";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-descubre',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, TruncateTextPipe, CommonModule],
   templateUrl: './descubre.component.html',
   styleUrl: './descubre.component.css'
 })
 export class DescubreComponent {
+
+  miscasas: Casas[] = [];
+
+  constructor(public miservicio: CasasService){
+    console.log("constructos de casas");
+  }
+
+  ngOnInit(): void{
+    console.log("ngOnInit");
+    this.miscasas = this.miservicio.getCasas();
+    console.log(this.miscasas);
+    
+  }
 
 }
