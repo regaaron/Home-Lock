26f5c6fffe6bdb584843f6299ae9b1d70043cd84
OmarReyesRmz <reyesomar028@gmail.com>
2024-04-25 14:04:04 -0600

    update reservas

--- src/app/componentes/calendario/calendario.component.ts
+++ src/app/componentes/calendario/calendario.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {JsonPipe} from '@angular/common';
+import {CommonModule, JsonPipe} from '@angular/common';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {provideNativeDateAdapter} from '@angular/material/core';
@@ -9,13 +9,48 @@ import {provideNativeDateAdapter} from '@angular/material/core';
   selector: 'app-calendario',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe],
+  imports: [MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe, CommonModule],
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.css'
 })
 export class CalendarioComponent {
+
+  
+  @Output() diasCalculados = new EventEmitter<number>();
+  startDate: Date | null = null;
+  endDate: Date | null = null;
+  dias: number | null = null;
+
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
+
+  getNumberOfDays(): void {
+    console.log('startDate:', this.startDate);
+    console.log('endDate:', this.endDate);
+
+    if (this.startDate && this.endDate) {
+      const startTimeStamp = this.startDate.getTime();
+      const endTimeStamp = this.endDate.getTime();
+
+      console.log('startTimeStamp:', startTimeStamp);
+      console.log('endTimeStamp:', endTimeStamp);
+
+      const differenceInMilliseconds = Math.abs(endTimeStamp - startTimeStamp);
+      console.log('differenceInMilliseconds:', differenceInMilliseconds);
+
+      const millisecondsInDay = 1000 * 60 * 60 * 24;
+      const differenceInDays = Math.floor(differenceInMilliseconds / millisecondsInDay);
+
+      console.log('differenceInDays:', differenceInDays);
+
+      this.dias = differenceInDays;
+    } else {
+      this.dias = 0;
+    }
+    this.diasCalculados.emit(this.dias);
+  }
+  
+
 }
--- src/app/componentes/reservas/reservas.component.ts
+++ src/app/componentes/reservas/reservas.component.ts
@@ -1,24 +1,51 @@
 import { Component, Input } from '@angular/core';
 import { Casas } from '../../casas';
 import { CasasService } from '../../servicios/casas.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CalendarioComponent } from '../calendario/calendario.component';
 
 @Component({
   selector: 'app-reservas',
   standalone: true,
-  imports: [],
+  imports: [CalendarioComponent,RouterLink],
   templateUrl: './reservas.component.html',
   styleUrl: './reservas.component.css'
 })
 export class ReservasComponent {
   
   @Input() casa!: Casas;
+  id: number = 0;
   constructor(public casaService : CasasService, public activatedRoute: ActivatedRoute){
     activatedRoute.params.subscribe(
       params =>{
         this.casa = casaService.getUnaCasa(params['ids']);
+        this.id = params['ids'];
       }
     );
+  }
+  
+  dias!: number;
+  totalxnoche: number = 0;
+  tarifa: number = 0;
+  tarifa2: number = 0;
+  impuestos: number = 0;
+  sumatotal: number = 0;
+
+  calculardias(numero : number){
+    this.dias = numero;
+    this.calculargastos(this.dias);
+  }
+
+  calculargastos(dias:number){
+    this.totalxnoche = this.casa.precio *  dias;
+    if(this.totalxnoche < 15000){
+      this.impuestos = this.totalxnoche * .15;
+      this.tarifa = this.totalxnoche * .15;
+      this.tarifa2 = this.totalxnoche * .30;
+    }else{
+      this.tarifa = this.totalxnoche * .45;
+    }
+    this.sumatotal = this.totalxnoche + this.tarifa + this.tarifa2 + this.impuestos;  
   }
   
 }
