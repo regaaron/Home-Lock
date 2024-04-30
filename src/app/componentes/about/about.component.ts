faf4ba217df0217e0386a2822acba6d120fe3936
OmarReyesRmz <reyesomar028@gmail.com>
2024-04-30 07:06:15 -0600

    ngClass

--- src/app/componentes/about/about.component.ts
+++ src/app/componentes/about/about.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  // isActive: boolean = true;
+  // isError: boolean = false;
+  // isPrimary: boolean = true;
 
+  // // Función para cambiar el estado de isActive
+  // toggleActive() {
+  //   this.isActive = !this.isActive;
+  // }
+
+  // // Función para cambiar el estado de isError
+  // toggleError() {
+  //   this.isError = !this.isError;
+  // }
+  // NgClass
 }
