65786674c9d1f34bacbf06c12dbbb4d4f625e6dd
eljohan <al262658@edu.uaa.mx>
2024-04-30 23:46:36 -0600

    masonry terminado

--- src/app/componentes/descubre/descubre.component.ts
+++ src/app/componentes/descubre/descubre.component.ts
@@ -5,18 +5,21 @@ import { RouterModule } from '@angular/router';
 import { TruncateTextPipe } from "../../truncate-text.pipe";
 import { CommonModule } from '@angular/common';
 import { DomseguroPipe } from '../../domseguro.pipe';
+import { MYImage } from '../../interfaces/myimage';
+import { MImageService } from '../../servicios/mimage.service';
 
 
 @Component({
   selector: 'app-descubre',
   standalone: true,
-  imports: [RouterModule, TruncateTextPipe, CommonModule,DomseguroPipe],
+  imports: [RouterModule, TruncateTextPipe, CommonModule,DomseguroPipe,],
   templateUrl: './descubre.component.html',
   styleUrl: './descubre.component.css'
 })
 export class DescubreComponent {
 
   miscasas: Casas[] = [];
+  misimagenes: MYImage[]=[];
 
   constructor(public miservicio: CasasService){
     console.log("constructos de casas");
