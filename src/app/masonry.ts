70bd9999a6051a130c1232373e8ee6d3f0d08979
OmarReyesRmz <reyesomar028@gmail.com>
2024-05-02 19:40:49 -0600

    masonry

---
+++ src/app/masonry.ts
@@ -0,0 +1,430 @@
+import { MYImage } from "./interfaces/myimage";
+
+export const MASON: MYImage[] = [
+    {
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Comedor",
+        image: "http://localhost:4200/assets/casas/casa1/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa1/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Balcón",
+        image: "http://localhost:4200/assets/casas/casa1/img1.webp"
+    },
+    {
+        tipo: 3,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa2/img1.webp"
+    },
+    {
+        tipo: 2,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa2/img5.webp"
+    },
+    {
+        tipo: 2,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "comedor",
+        image: "http://localhost:4200/assets/casas/casa2/img2.webp"
+    },
+    {
+        tipo: 2,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Sala de estar",
+        image: "http://localhost:4200/assets/casas/casa2/img3.webp"
+    },
+    {
+        tipo: 2,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Comedor",
+        image: "http://localhost:4200/assets/casas/casa2/img4.webp"
+    },
+    {
+        tipo: 2,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "comedor",
+        image: "http://localhost:4200/assets/casas/casa3/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "Vista al mar",
+        image: "http://localhost:4200/assets/casas/casa3/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa3/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa3/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa3/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Cuernavaca",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa4/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Cuernavaca",
+        recamara: "Habitacion",
+        image: "http://localhost:4200/assets/casas/casa4/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Cuernavaca",
+        recamara: "Sala de juegos",
+        image: "http://localhost:4200/assets/casas/casa4/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Cuernavaca",
+        recamara: "Cancha de tenis",
+        image: "http://localhost:4200/assets/casas/casa4/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Cuernavaca",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa4/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Alojamiento en Indialantic",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa16/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Alojamiento en Indialantic",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa16/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Alojamiento en Indialantic",
+        recamara: "Salida al mar",
+        image: "http://localhost:4200/assets/casas/casa16/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Alojamiento en Indialantic",
+        recamara: "Sala de estar",
+        image: "http://localhost:4200/assets/casas/casa16/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Alojamiento en Indialantic",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa16/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Departamento en Puerto Vallarta",
+        recamara: "Habitacion",
+        image: "http://localhost:4200/assets/casas/casa13/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Departamento en Puerto Vallarta",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa13/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Departamento en Puerto Vallarta",
+        recamara: "Balcón",
+        image: "http://localhost:4200/assets/casas/casa13/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Departamento en Puerto Vallarta",
+        recamara: "Balcón",
+        image: "http://localhost:4200/assets/casas/casa13/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Departamento en Puerto Vallarta",
+        recamara: "Vista al mar",
+        image: "http://localhost:4200/assets/casas/casa13/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda en Cancun",
+        recamara: "Balneario",
+        image: "http://localhost:4200/assets/casas/casa14/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda en Cancun",
+        recamara: "Balcón",
+        image: "http://localhost:4200/assets/casas/casa14/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda en Cancun",
+        recamara: "Habitacion",
+        image: "http://localhost:4200/assets/casas/casa14/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda en Cancun",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa14/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Vivienda en Cancun",
+        recamara: "Comedor",
+        image: "http://localhost:4200/assets/casas/casa14/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Flager Beach",
+        recamara: "Sala de estar",
+        image: "http://localhost:4200/assets/casas/casa15/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Flager Beach",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa15/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Flager Beach",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa15/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Flager Beach",
+        recamara: "Sala de estar",
+        image: "http://localhost:4200/assets/casas/casa15/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Flager Beach",
+        recamara: "Sala de juegos",
+        image: "http://localhost:4200/assets/casas/casa15/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Santo Domingo, Ocotitlan",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa9/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Santo Domingo, Ocotitlan",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa9/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Santo Domingo, Ocotitlan",
+        recamara: "Balcón",
+        image: "http://localhost:4200/assets/casas/casa9/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Santo Domingo, Ocotitlan",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa9/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Santo Domingo, Ocotitlan",
+        recamara: "Sala de estar",
+        image: "http://localhost:4200/assets/casas/casa9/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Ciudad de Mexico",
+        recamara: "Baño",
+        image: "http://localhost:4200/assets/casas/casa10/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Ciudad de Mexico",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa10/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Ciudad de Mexico",
+        recamara: "Comedor",
+        image: "http://localhost:4200/assets/casas/casa10/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Ciudad de Mexico",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa10/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Ciudad de Mexico",
+        recamara: "Habitacion",
+        image: "http://localhost:4200/assets/casas/casa10/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Cuernavaca, Mexico ",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa11/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Cuernavaca, Mexico ",
+        recamara: "Baño",
+        image: "http://localhost:4200/assets/casas/casa11/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Cuernavaca, Mexico ",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa11/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Cuernavaca, Mexico ",
+        recamara: "Sala de estar",
+        image: "http://localhost:4200/assets/casas/casa11/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Cuernavaca, Mexico ",
+        recamara: "comedor",
+        image: "http://localhost:4200/assets/casas/casa11/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Guadalajara, Mexico",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa12/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Guadalajara, Mexico",
+        recamara: "Baño",
+        image: "http://localhost:4200/assets/casas/casa12/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Guadalajara, Mexico",
+        recamara: "Jardin",
+        image: "http://localhost:4200/assets/casas/casa12/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Guadalajara, Mexico",
+        recamara: "Cocina",
+        image: "http://localhost:4200/assets/casas/casa12/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Malibu, California",
+        recamara: "Sala de estar",
+        image: "http://localhost:4200/assets/casas/casa17/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Malibu, California",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa17/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Malibu, California",
+        recamara: "Jacuzzi",
+        image: "http://localhost:4200/assets/casas/casa17/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Malibu, California",
+        recamara: "Balcón",
+        image: "http://localhost:4200/assets/casas/casa17/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Suite, Playa del Carmen",
+        recamara: "Vista al mar",
+        image: "http://localhost:4200/assets/casas/casa18/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Suite, Playa del Carmen",
+        recamara: "Cenote",
+        image: "http://localhost:4200/assets/casas/casa18/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Suite, Playa del Carmen",
+        recamara: "Habitacion",
+        image: "http://localhost:4200/assets/casas/casa18/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Suite, Playa del Carmen",
+        recamara: "Aaron Ruiz",
+        image: "http://localhost:4200/assets/casas/casa18/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Suite, Playa del Carmen",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa18/img4.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Costa Esmeralda",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa19/img1.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Costa Esmeralda",
+        recamara: "Vista completa",
+        image: "http://localhost:4200/assets/casas/casa19/img5.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Costa Esmeralda",
+        recamara: "Vista al mar",
+        image: "http://localhost:4200/assets/casas/casa19/img3.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Costa Esmeralda",
+        recamara: "Vista al mar",
+        image: "http://localhost:4200/assets/casas/casa19/img2.webp"
+    },
+    {
+        tipo: 1,
+        casa: "Residencia Costa Esmeralda",
+        recamara: "Piscina",
+        image: "http://localhost:4200/assets/casas/casa19/img4.webp"
+    }
+];
--- src/app/servicios/mimage.service.ts
+++ src/app/servicios/mimage.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { take } from 'rxjs';
+import { MYImage } from '../interfaces/myimage';
+import { MASON } from '../masonry';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,14 @@ import { take } from 'rxjs';
 export class MImageService {
 
   urlApi:string="https://masonryabnb.free.beeceptor.com";
+  
+  private mason: MYImage[] = MASON;
 
   constructor(private http: HttpClient) { }
+
+  getMason() : MYImage[]{
+    return this.mason;
+  }
 
   retornar(){
 
