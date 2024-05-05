52fac72a7a5700e0c6ab0a380d13ee29f65948c5
regaaron <aaron_lopez222@hotmail.com>
2024-05-05 10:30:36 -0600

    massonry.ts

--- src/app/masonry.ts
+++ src/app/masonry.ts
@@ -2,191 +2,191 @@ import { MYImage } from "./interfaces/myimage";
 
 export const MASON: MYImage[] = [
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Comedor",
-        "image": "http://localhost:4200/assets/Masonry/1/img2.jpg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Comedor",
+        image: "assets/Masonry/1/img2.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Vista completa",
-        "image": "http://localhost:4200/assets/Masonry/1/img1.jpg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Vista completa",
+        image: "assets/Masonry/1/img1.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Balcón",
-        "image": "http://localhost:4200/assets/Masonry/1/img3.jpg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Balcón",
+        image: "assets/Masonry/1/img3.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Vista completa",
-        "image": "http://localhost:4200/assets/Masonry/1/img4.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Vista completa",
+        image: "assets/Masonry/1/img4.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Cocina",
-        "image": "http://localhost:4200/assets/Masonry/1/img5.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Cocina",
+        image: "assets/Masonry/1/img5.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/1/img6.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "comedor",
+        image: "assets/Masonry/1/img6.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Sala de estar",
-        "image": "http://localhost:4200/assets/Masonry/1/img7.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Sala de estar",
+        image: "assets/Masonry/1/img7.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Comedor",
-        "image": "http://localhost:4200/assets/Masonry/1/img8.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Comedor",
+        image: "assets/Masonry/1/img8.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Vivienda rentada en Cabo San Lucas",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/1/img9.jpg"
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "comedor",
+        image: "assets/Masonry/1/img9.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Vivienda rentada en Cabo San Lucas",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/1/img10.jpg"
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "comedor",
+        image: "assets/Masonry/1/img10.jpg"
     },
 
 
     /*Parte dos*/
 
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Comedor",
-        "image": "http://localhost:4200/assets/Masonry/3/img1.jpg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Comedor",
+        image: "assets/Masonry/3/img1.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Vista completa",
-        "image": "http://localhost:4200/assets/Masonry/3/img3.jpg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Vista completa",
+        image: "assets/Masonry/3/img3.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Balcón",
-        "image": "http://localhost:4200/assets/Masonry/3/img3.jpg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Balcón",
+        image: "assets/Masonry/3/img3.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Vista completa",
-        "image": "http://localhost:4200/assets/Masonry/3/img4.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Vista completa",
+        image: "assets/Masonry/3/img4.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Cocina",
-        "image": "http://localhost:4200/assets/Masonry/3/img5.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Cocina",
+        image: "assets/Masonry/3/img5.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/3/img6.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "comedor",
+        image: "assets/Masonry/3/img6.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Sala de estar",
-        "image": "http://localhost:4200/assets/Masonry/3/img7.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Sala de estar",
+        image: "assets/Masonry/3/img7.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Comedor",
-        "image": "http://localhost:4200/assets/Masonry/3/img8.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Comedor",
+        image: "assets/Masonry/3/img8.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Vivienda rentada en Cabo San Lucas",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/3/img9.jpg"
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "comedor",
+        image: "assets/Masonry/3/img9.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Vivienda rentada en Cabo San Lucas",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/3/img10.jpg"
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "comedor",
+        image: "assets/Masonry/3/img10.jpg"
     },
 
     /*Parte tres*/
 
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Comedor",
-        "image": "http://localhost:4200/assets/Masonry/2/img1.png"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Comedor",
+        image: "assets/Masonry/2/img1.png"
     },
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Vista completa",
-        "image": "http://localhost:4200/assets/Masonry/2/img2.jpeg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Vista completa",
+        image: "assets/Masonry/2/img2.jpeg"
     },
     {
-        "tipo": 1,
-        "casa": "habitacion privada en loft Acapulco",
-        "recamara": "Balcón",
-        "image": "http://localhost:4200/assets/Masonry/2/img3.jpg"
+        tipo: 1,
+        casa: "habitacion privada en loft Acapulco",
+        recamara: "Balcón",
+        image: "assets/Masonry/2/img3.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Vista completa",
-        "image": "http://localhost:4200/assets/Masonry/2/img4.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Vista completa",
+        image: "assets/Masonry/2/img4.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Cocina",
-        "image": "http://localhost:4200/assets/Masonry/2/img5.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Cocina",
+        image: "assets/Masonry/2/img5.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/2/img6.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "comedor",
+        image: "assets/Masonry/2/img6.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Sala de estar",
-        "image": "http://localhost:4200/assets/Masonry/2/img7.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Sala de estar",
+        image: "assets/Masonry/2/img7.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Casa de campo en Tepoztlan",
-        "recamara": "Comedor",
-        "image": "http://localhost:4200/assets/Masonry/2/img8.jpg"
+        tipo: 1,
+        casa: "Casa de campo en Tepoztlan",
+        recamara: "Comedor",
+        image: "assets/Masonry/2/img8.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Vivienda rentada en Cabo San Lucas",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/2/img9.jpg"
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "comedor",
+        image: "assets/Masonry/2/img9.jpg"
     },
     {
-        "tipo": 1,
-        "casa": "Vivienda rentada en Cabo San Lucas",
-        "recamara": "comedor",
-        "image": "http://localhost:4200/assets/Masonry/2/img10.jpg"
+        tipo: 1,
+        casa: "Vivienda rentada en Cabo San Lucas",
+        recamara: "comedor",
+        image: "assets/Masonry/2/img10.jpg"
     },
 
 
